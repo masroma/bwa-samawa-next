4a8836f1fe56cbcf9450733eba3384feba26c725
masroma75 <masroma75@gmail.com>
2024-12-12 05:33:49 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'localhost:8000',
+            port: '',
+            pathname: '/api/**',
+            search: '',
+          },
+        ],
+      },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };
 
 export default nextConfig;
